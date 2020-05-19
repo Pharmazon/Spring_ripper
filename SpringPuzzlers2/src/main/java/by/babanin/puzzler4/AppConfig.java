032e245b724de4b1e6dc5a0a16297acef93adde8
Alexey Shcheglov <Shcheglov.A.S@sberbank.ru>
2020-05-20 01:05:53 +0400

    Maven project structure unified

---
+++ SpringPuzzlers2/src/main/java/by/babanin/puzzler4/AppConfig.java
@@ -0,0 +1,26 @@
+package by.babanin.puzzler4;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class AppConfig {
+    @Value("${JAVA_HOME}")
+    private String javaHome;
+
+    @PostConstruct
+    public void init() {
+        System.out.println(javaHome);
+    }
+    /*
+    Warning:
+    PropertySourcesPlaceholderConfigurer являеться BeanFactoryPostProcessor и плохо,
+    что ты его создаешь не через статический метод
+     */
+    /* @Bean
+    public PropertySourcesPlaceholderConfigurer configurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }*/
+}
